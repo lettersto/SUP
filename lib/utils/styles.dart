ac5b2c0d2c1f42cc37062792deadbea269d5d5a8
정혜령 <jhr3681@naver.com>
2023-03-06 16:37:01 +0900

    [SETTING] #1: change app color file name

---
+++ lib/utils/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // ============ grayscale ===========
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const grayTransparent = Color.fromRGBO(176, 176, 176, .1);
+  static const gray = Color.fromRGBO(139, 139, 139, 1);
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+
+  // ============== pink ==============
+  static const pink10 = Color.fromRGBO(255, 245, 245, 1);
+  static const pink20 = Color.fromRGBO(255, 210, 227, 1);
+  static const pink30 = Color.fromRGBO(250, 173, 202, 1);
+  static const pink40 = Color.fromRGBO(255, 133, 188, 1);
+  static const pink60 = Color.fromRGBO(216, 0, 112, 1);
+  static const pinkAccent = Color.fromRGBO(255, 2, 154, 1);
+
+  const AppColors();
+}

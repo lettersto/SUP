5a580baa28a62a7dd9a5e881c2425e0f062de97a
정혜령 <jhr3681@naver.com>
2023-03-06 16:00:43 +0900

    [SETTING] #1 : add app base color

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // ============ grayscale ===========
+  final white = const Color.fromRGBO(255, 255, 255, 1);
+  final grayTransparent = const Color.fromRGBO(176, 176, 176, .1);
+  final gray = const Color.fromRGBO(139, 139, 139, 1);
+  final black = const Color.fromRGBO(0, 0, 0, 1);
+
+  // ============== pink ==============
+  final pink10 = const Color.fromRGBO(255, 245, 245, 1);
+  final pink20 = const Color.fromRGBO(255, 210, 227, 1);
+  final pink30 = const Color.fromRGBO(250, 173, 202, 1);
+  final pink40 = const Color.fromRGBO(255, 133, 188, 1);
+  final pink60 = const Color.fromRGBO(216, 0, 112, 1);
+  final pinkAccent = const Color.fromRGBO(255, 2, 154, 1);
+
+  const AppColors();
+}

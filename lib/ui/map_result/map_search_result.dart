1e6ad411ffc0e2715793cb98a11e3446e8f6b87e
YunaSong97 <unme97@naver.com>
2023-03-22 05:08:15 +0900

    [FIX] #51 : 스크롤 높이

--- lib/ui/map_result/map_search_result.dart
+++ lib/ui/map_result/map_search_result.dart
@@ -189,7 +189,7 @@ class MapResultPageState extends ConsumerState<MapResultPage> {
                 child: DraggableScrollableSheet(
                     initialChildSize: stores.isEmpty ? 0 : 0.35,
                     minChildSize: stores.isEmpty ? 0 : 0.35,
-                    maxChildSize:
+                    ㅎmaxChildSize:
                         min(itemHeight * stores.length + headerHeight, 1),
                     expand: false,
                     builder: (BuildContext context,
